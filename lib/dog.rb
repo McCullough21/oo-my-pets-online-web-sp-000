e1bccc95137b6feaf796638fd19bfc44102d5205
Michael McCullough <michael.mccullough21@gmail.com>
2019-07-10 18:18:42 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -12,10 +12,11 @@ end
 def name
   @name
 end
-# def walk_dogs
-#     dog.mood = "happy"
-#   end
-# end
+def walk_dogs
+  # @owner.each do |dog|
+    dog.mood = "happy"
+  # end
+end
 
 def self.all
   @@all
